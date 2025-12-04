33f2e315d3fd01e78170ac983b27dc6c2202fd13
jyotiradityajangir <149015206+jyotiradityajangir@users.noreply.github.com>
2025-12-04 10:15:56 -0500

    final

--- LAB04/routes/index.js
+++ LAB04/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The Beatles - Home' });
+});
+
+/* GET John Lennon page */
+router.get('/john', function(req, res, next) {
+  res.render('john', { title: 'John Lennon' });
+});
+
+/* GET Paul McCartney page */
+router.get('/paul', function(req, res, next) {
+  res.render('paul', { title: 'Paul McCartney' });
+});
+
+/* GET George Harrison page */
+router.get('/george', function(req, res, next) {
+  res.render('george', { title: 'George Harrison' });
+});
+
+/* GET Ringo Starr page */
+router.get('/ringo', function(req, res, next) {
+  res.render('ringo', { title: 'Ringo Starr' });
 });
 
 module.exports = router;
